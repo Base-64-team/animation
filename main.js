4b24811b7549f65dc22dde7d46db1447be141269
Base-64-team <windfrosty0@gmail.com>
2024-08-31 10:41:56 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-var hjj = document.getElementById('animationType');
-
-function jogg() {
-        hjj.classList.add("hello");
-}
+var box = document.getElementById('box')
+document.getElementById("submit").addEventListener("click", function() {
+        var box = document.getElementById('box');
+        var type = document.getElementById('animationType').value;
+        var time = document.getElementById('animationTime').value;
+        var angle = document.getElementById('rotationAndMore').value;
+        box.classList.remove('ease', 'ease-in', 'ease-out', 'ease-in-out', 'linear', 'deg0', 'deg90', 'deg180', 'deg360', 'sec1', 'sec3', 'sec6', 'sec0-05', 'sec0-5', 'sec0-1')
+        box.classList.add(type, time, angle)
+})
